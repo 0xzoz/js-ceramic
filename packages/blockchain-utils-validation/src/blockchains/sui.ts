f7c212bfc1a31198861c6839282fbd0c83914545
0xzoz <zeroexzoz@gmail.com>
2023-01-04 21:12:43 -0800

    Add Sui wallet support: first pass

--- packages/blockchain-utils-validation/src/blockchains/sui.ts
+++ packages/blockchain-utils-validation/src/blockchains/sui.ts
@@ -1,23 +1,28 @@
 import { BlockchainHandler } from '../blockchain-handler.js'
 import { LinkProof } from '@ceramicnetwork/blockchain-utils-linking'
 import * as uint8arrays from 'uint8arrays'
-import { verify } from '@stablelib/ed25519'
 import { normalizeAccountId } from '@ceramicnetwork/common'
+import nacl from 'tweetnacl';
+import { Base64DataBuffer } from "@mysten/sui.js";
 
 const verifySignature = async (
   pubKey: Uint8Array,
   message: string,
   signature: Uint8Array
 ): Promise<boolean> => {
-  const verified = verify(pubKey, uint8arrays.fromString(message), signature)
+  const signData = new Base64DataBuffer(new TextEncoder().encode(message));
+  const verified = nacl.sign.detached.verify(
+    signData.getData(),
+    signature,
+    pubKey
+  )
   return verified
 }
 
 const namespace = 'sui'
 
 export async function validateLink(proof: LinkProof): Promise<LinkProof | null> {
-  const account = normalizeAccountId(proof.account)
-  const pubKey = uint8arrays.fromString(account.address, 'base58btc')
+  const pubKey = uint8arrays.fromString(normalizeAccountId(proof.account).address, 'base64')
   const msg = proof.message
   const sig = uint8arrays.fromString(proof.signature, 'base64')
   const is_sig_valid = await verifySignature(pubKey, msg, sig)
